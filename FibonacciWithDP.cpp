6f61ec2b0d77fe6bc281e0d69bdf1986a44901e4
wkwkwill <willykomara@gmail.com>
2019-12-12 23:36:52 +0700

    fix fib with DP

--- FibonacciWithDP.cpp
+++ FibonacciWithDP.cpp
@@ -1,16 +1,44 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+unsigned long long int memo[10000];
+
 
 unsigned long long int Fib(long int a)
 {
+    if (memo[a] != 0)
+       return memo[a];
     if (a <= 1)
         return a;
-    return Fib(a-1) + Fib(a-2);
+    unsigned long long int fib = Fib(a-1) + Fib(a-2);
+    memo[a] = fib;
+    //scout << memo[12] << endl;
+    return fib;
 }
+
 int main()
 {
-    int indexFib = 50;
+    memset(memo, 0, sizeof(memo));
+    int indexFib = 60;
     cout << Fib(indexFib);
     return 0;
 }
+
+
+/*
+int main()
+{
+    long int bef = 0;
+    long int n = 46, nilaiFib = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (i > 0)
+        {
+            int temp = nilaiFib;
+            nilaiFib += bef;
+            bef = temp;;
+        }   
+    }
+    cout << nilaiFib << endl;
+}
+*/
