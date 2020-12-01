ce36a8aa7ed548a0727f3f5cdb5ec66aa91f8d6a
wkwkwill <willykomara@gmail.com>
2020-12-01 11:33:53 +0700

    INC 2020 - Problem I + Template

--- MY TEMPLATE/TEMPLATE_2020_NOV.cpp
+++ MY TEMPLATE/TEMPLATE_2020_NOV.cpp
@@ -123,70 +123,9 @@ int gcd(int a, int b)
 
 /*===================== SOLUTION =====================*/
 
-map<ll, int> variety;
-vector<int> possible;
-vector<int> dp(250005, 0);
-vector<ll> a(505);
-
-const ll maks = 1e12;
-
-ll countVariety(ll x, ll y)
-{
-    if (x == y) return maks / x - 1;
-    ll fpb = gcd(x, y);
-    ll kpk = (x * y) / fpb;
-    ll sp = max((kpk + 1), (x + y)); //starting point
-    ll bb = x + y - 1; //batas bawah
-    ll ans = (maks / fpb) - ((sp-1) / fpb);
-    ans += max(0LL, (sp-2) - bb);
-    ans -= max(0LL, ((sp-2) / x) - (bb / x));
-    ans -= max(0LL, ((sp-2) / y) - (bb / y));
-    return ans;
-}
-
-void initDP(int x)
-{
-    int n = possible.size();
-    int maksSum = x;
-    dp[0] = 1;
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = x; j >= possible[i]; j--)
-        {
-            dp[j] += dp[j - possible[i]];
-        }
-    }
-}
-
 void solve()
 {
-    int n, q;
-    cin >> n >> q;
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> a[i];
-    }
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = i+1; j < n; ++j)
-        {
-            ll count = countVariety(a[i], a[j]);
-            cout << "(" << i + 1 << ", "  << j + 1 << ") = " << count << '\n';
-            variety[count]++;
-        }
-    }
-    for (auto &i : variety)
-    {
-        possible.push_back(i.second);
-        cout << i.first << ' ' << i.second << '\n';
-    }
-    initDP(n * (n-1) / 2);
-    for (int i = 0; i < q; ++i)
-    {
-        int query;
-        cin >> query;
-        dp[query] > 0 ? cout << "YES\n" : cout << "NO\n";
-    }
+    
 }
  
 /* ==================== KOTRETAN ===================== *\
